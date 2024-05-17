9d80a4829eec22be797c7cb0ed20c6ae81f36c73
niltonslf <niltonrck@gmail.com>
2024-05-17 07:08:08 -0300

    feat: add 2 screens in the viewport

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,73 @@
+import {useEffect, useState} from 'react'
+import {DeviceScreen} from './components'
+
+const screens = [
+  {
+    id: 'mobile-generic',
+    width: 414,
+    height: 896,
+  },
+  {
+    id: 'desktop-generic',
+    width: 1440,
+    height: 900,
+  },
+
+  {
+    id: 'tablet-generic',
+    width: 960,
+    height: 900,
+  },
+]
+
 function App() {
-  return <main className='w-full bg-neutral'></main>
+  const [posY, setPosY] = useState(0)
+  const [posX, setPosX] = useState(0)
+
+  useEffect(() => {
+    const screens = document.querySelectorAll('[id^=screen-]')
+
+    console.log({screens})
+
+    screens.forEach((screen, index) => {
+      if (index === 0) {
+        screen.style.left = '40px'
+      } else {
+        const prevScreen = screens[index - 1]
+        const newPos = prevScreen.offsetLeft + prevScreen.offsetWidth + 40
+        screen.style.left = `${newPos}px`
+      }
+    })
+  }, [])
+
+  useEffect(() => {
+    document.addEventListener('wheel', scroll => {
+      setPosX(prev => prev + scroll.wheelDeltaX)
+      setPosY(prev => prev + scroll.wheelDeltaY)
+    })
+  }, [])
+
+  return (
+    <main className='w-full bg-neutral h-full flex overflow-hidden'>
+      {/* CANVAS */}
+      <div className='flex min-w-full h-screen overflow-hidden relative'>
+        {/* VIEWPORT */}
+        <div
+          style={{
+            transform: `translate3d(${posX}px, ${posY}px, 0px)`,
+          }}>
+          {screens.map((screen, index) => (
+            <DeviceScreen
+              key={screen.id}
+              src='http://localhost:3000/app'
+              screen={screen}
+              prevScreen={screens[index - 1]}
+            />
+          ))}
+        </div>
+      </div>
+    </main>
+  )
 }
 
 export default App
---
+++ src/components/devices/device-screen.tsx
@@ -0,0 +1,40 @@
+import {merge} from '../../utils'
+
+type Screen = {
+  id: string
+  width: number
+  height: number
+}
+type Props = {
+  src: string
+  screen: Screen
+  prevScreen: Screen | undefined
+}
+
+export const DeviceScreen = ({src, screen, prevScreen}: Props) => {
+  return (
+    <div
+      id={`screen-${screen.id}`}
+      className={merge('bg-white flex flex-col absolute')}
+      style={{
+        width: screen.width,
+        height: screen.height,
+        top: '40px',
+        // transform: 'scale(0.8)',
+        // transformOrigin: '0px 0px',
+      }}>
+      <div
+        className='h-full w-full overflow-hidden'
+        style={{
+          width: screen.width,
+          height: screen.height,
+        }}>
+        <iframe
+          src={src}
+          sandbox='allow-scripts allow-forms allow-same-origin allow-presentation'
+          className='w-full h-full'
+          style={{height: screen.height}}></iframe>
+      </div>
+    </div>
+  )
+}
