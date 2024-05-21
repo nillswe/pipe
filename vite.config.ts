bb5c4d1f5104ec6dd711a83e52ccb16138171fd0
niltonslf <niltonrck@gmail.com>
2024-05-21 08:11:19 -0300

    feat: init chrome extension

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import {defineConfig} from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
+import {crx} from '@crxjs/vite-plugin'
+import manifest from './manifest.json'
+
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), crx({manifest})],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
