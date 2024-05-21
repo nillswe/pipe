95ba4afcc5479f1646d444747a621ef92c311fe0
niltonslf <niltonrck@gmail.com>
2024-05-21 05:15:01 -0300

    test: ensure to add a new device when clicked on its icon

--- src/components/devices-modal/devices-modal.test.tsx
+++ src/components/devices-modal/devices-modal.test.tsx
@@ -1,19 +1,39 @@
 import {DevicesModal} from '@/components/devices-modal'
-import {render, screen} from '@testing-library/react'
-import {describe, expect, it} from 'vitest'
+import {appStore} from '@/store/app-store'
+import {cleanup, render, screen} from '@testing-library/react'
+import {beforeAll, describe, expect, it} from 'vitest'
+
+import userEvent from '@testing-library/user-event'
 
 const makeSUT = () => {
+  const user = userEvent.setup()
   const sut = render(<DevicesModal isOpen={true} onClose={() => null} />)
 
-  return {sut}
+  return {sut, user}
 }
 
 describe('DeviceModal', () => {
+  beforeAll(() => {
+    cleanup()
+  })
+
   it('should list all available devices', () => {
     makeSUT()
 
     const dialogContent = screen.getByTestId('modal-content')
 
     expect(dialogContent.children.length).not.toBe(0)
+  })
+
+  it('should add a new device to screen when click on it', async () => {
+    const {user} = makeSUT()
+    const initialDevicesAmount = appStore.devices.length
+
+    const dialogContent = screen.getByTestId('modal-content')
+    const firstDevice = dialogContent.children[0]
+
+    await user.click(firstDevice)
+
+    expect(appStore.devices.length).toBe(initialDevicesAmount + 1)
   })
 })
