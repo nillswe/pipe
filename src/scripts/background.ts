9d65f2389546a42c1b4db3dbad9ad588d011de0d
niltonslf <niltonrck@gmail.com>
2024-05-22 04:08:24 -0300

    fix: address the issue of not load init script

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -1,10 +1,12 @@
 ///<reference types="chrome"/>
+// @ts-ignore
+import init from '@/scripts/init?script'
 
 chrome.action.onClicked.addListener(async tab => {
   if (!tab.id) return
 
   await chrome.scripting.executeScript({
     target: {tabId: tab.id},
-    files: ['scripts/init.ts'],
+    files: [init],
   })
 })
