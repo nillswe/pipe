2f5a1732ece9308eed3ac0026faf2ea0287036d6
niltonslf <niltonrck@gmail.com>
2024-05-21 23:08:45 -0300

    feat: add button to add device when there isn't devices yet

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,15 +1,18 @@
-import {DeviceScreen} from '@/components'
+import {DeviceScreen, DevicesModal} from '@/components'
 import {ViewportContainer, ZoomContainer} from '@/components/containers'
 import {Sidebar} from '@/components/sidebar'
 import {appStore} from '@/store/app-store'
 import {appUiStore} from '@/store/app-ui-store'
-import {Info} from 'lucide-react'
+import {useToggle} from '@uidotdev/usehooks'
+import {MonitorSmartphone} from 'lucide-react'
 import {observer} from 'mobx-react-lite'
 import {useEffect} from 'react'
 
 export const MainPage = observer(() => {
   const {devices} = appStore
   const {scale} = appUiStore
+
+  const [isDevicesModalOpen, toggleDevicesModal] = useToggle(false)
 
   useEffect(() => {
     window.addEventListener('wheel', event => appUiStore.scrollPage(event))
@@ -31,15 +34,21 @@ export const MainPage = observer(() => {
       className='w-screen relative bg-base h-screen flex overflow-hidden'
       onWheel={event => appUiStore.onZoom(event)}>
       <Sidebar />
+      <DevicesModal isOpen={isDevicesModalOpen} onClose={toggleDevicesModal} />
 
       <div
         id='content-container'
         className='flex w-full h-screen overflow-hidden relative'>
         {devices.length === 0 && (
           <div className='items-center justify-center flex w-screen h-full'>
-            <div className='alert alert-info w-9/12'>
-              <Info size={22} />
-              <span>Please, add your first device.</span>
+            <div className='flex flex-col gap-3'>
+              <span className='text-1xl'>Please, add your first device.</span>
+              <button
+                className='btn btn-primary'
+                onClick={() => toggleDevicesModal()}>
+                <MonitorSmartphone size={22} />
+                Add device
+              </button>
             </div>
           </div>
         )}
