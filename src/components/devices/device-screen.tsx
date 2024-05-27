83d1e8e90e80c7a9867b2485bffc04b3b5521127
niltonslf <niltonrck@gmail.com>
2024-05-26 23:00:31 -0300

    feat: basic implementation with bugs

--- src/components/devices/device-screen.tsx
+++ src/components/devices/device-screen.tsx
@@ -5,6 +5,7 @@ import {appStore} from '@/store/app-store'
 import {getIframeId, getScreenId, merge} from '@/utils'
 import {useToggle} from '@uidotdev/usehooks'
 import {syncLocationStore} from '@/store/sync-location-store'
+import {syncScrollStore} from '@/store/sync-scroll-store'
 
 type Props = {
   src: string
@@ -53,13 +54,17 @@ export const DeviceScreen = observer(({src, device: device}: Props) => {
         }}>
         {src ? (
           <iframe
+            scrolling='yes'
             id={getIframeId(device.id)}
-            onLoad={() => syncLocationStore.syncLocation(device)}
+            onLoad={() => {
+              syncLocationStore.syncLocation(device)
+              syncScrollStore.syncScroll(device)
+            }}
             src={src}
             sandbox={`allow-scripts allow-forms allow-same-origin allow-presentation allow-orientation-lock allow-modals allow-popups allow-popups-to-escape-sandbox allow-pointer-lock allow-top-navigation`}
             allow='web-share'
             className={merge([
-              'w-full h-full border-2 border-base-300 pointer-events-none',
+              'w-full h-full border-2 border-base-300 pointer-events-none overflow-auto',
               isFrameActive && 'pointer-events-auto border-primary border-2',
             ])}
             style={{height: device.height}}
---
+++ src/store/sync-scroll-store.ts
@@ -0,0 +1,43 @@
+import {Device} from '@/domain/models'
+import {isWebApp} from '@/platforms'
+import {getIframeContentDoc, getIframeDocElement, getIframeElem} from '@/utils'
+import {makeAutoObservable} from 'mobx'
+
+class SyncScrollStore {
+  isSyncScrollOn: boolean = true
+  constructor() {
+    makeAutoObservable(this)
+
+    if (isWebApp()) {
+      this.isSyncScrollOn = false
+    }
+  }
+
+  syncScroll(device: Device) {
+    const iframe = getIframeElem(device.id)!
+
+    window.addEventListener('message', event => {
+      if (event.data?.message === 'FRAME_SCROLL') {
+        if (event.data?.deviceIdOrigin !== device.id) {
+          getIframeDocElement(iframe)?.scrollTo({
+            top: event.data.data.top,
+            left: event.data.data.left,
+          })
+        }
+      }
+    })
+
+    getIframeContentDoc(iframe)?.addEventListener('scroll', () => {
+      window.postMessage({
+        message: 'FRAME_SCROLL',
+        deviceIdOrigin: device.id,
+        data: {
+          top: getIframeDocElement(iframe)?.scrollTop ?? 0,
+          left: getIframeDocElement(iframe)?.scrollLeft ?? 0,
+        },
+      })
+    })
+  }
+}
+
+export const syncScrollStore = new SyncScrollStore()
